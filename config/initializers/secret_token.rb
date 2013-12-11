1323f8f6bbea50df72c603c329ce38cc57dbcf26
juan pablo amaya <jp.amaya@codescrum.com>
2013-12-11 13:02:28 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = 'c626e5221d7ad05a01cbe93c48580002c0865f953c0423e611ab383712aa0675ef0b27ea5aa775a562545e8d4c4a28b81e23a40a0a02bd4a3c005a4c5a05f900'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Tienda::Application.routes.draw do
+  root :to => 'home#index'
+end
